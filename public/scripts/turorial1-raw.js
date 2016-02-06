a88533cfd9fbf46ce5f138af2044f1721a236e75
yufansun <yufansun@umich.edu>
2016-02-06 16:38:10 -0500

    first try

---
+++ public/scripts/turorial1-raw.js
@@ -0,0 +1,14 @@
+// turorial1-raw.js
+var CommentBox = React.createClass({displayName: 'CommentBox',
+render: function(){
+	return (
+		React.createElement('div',{className:"CommentBox"},
+			"Hello, world! I am a CommentBox."
+			)
+		);
+}
+});
+ReactDOM.render(
+	React.createElement(CommentBox,null),
+	document.getElementById('content')
+);
---
+++ public/scripts/tutorial1.js
@@ -0,0 +1,48 @@
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
+
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+// tutorial3.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
