c062c84da7532a2e474987ca2264906e216a073c
yufansun <yufansun@umich.edu>
2016-03-07 23:08:47 -0500

    Can send chat msg

--- public/status.js
+++ public/status.js
@@ -1,18 +1,24 @@
 var io = require("./server");
 var stable = require("./stable")
 console.log("Server running!")
+var contactStatus = [
+	{id: 1, Name: "Ren Lu",Status: "true"},
+	{id: 2, Name: "Qinye Li",Status: "false"}
+];
 io.on('connection', function(socket){
 	console.log("On connection");
-	socket.emit('initial',{id: 1, Name: "Ren Lu",Status: "true"});
-	socket.on('seen',function(coming_id){
-		console.log("Received request");
-		console.log("From "+coming_id);
+	//socket.emit('initial',{id: 1, Name: "Ren Lu",Status: "true"});
+	socket.on('myID',function(name){
+		console.log("Received request from "+name);
 		var online = stable.has(Number(coming_id));
-		socket.emit('initialStatus',{id: 0, Name: "Yufan",Status: "true"});
-		socket.emit('updateStatus',{id: 0, Name:"Yufan",Status: "false"});
-	})
+		socket.emit('initialStatus',contactStatus);
+		console.log("Initial Status sent!")
+	});
 	socket.on('1',function(Name){
 		console.log("1: "+Name);
-	})
-})
+	});
+	socket.on('chat message',function(msg){
+		console.log("Received chat message: "+msg);
+	});
+});
 
--- public/upload/InputBox.js
+++ public/upload/InputBox.js
@@ -1,3 +1,4 @@
+
 var InputBox = React.createClass({
   render: function(){
     return (
