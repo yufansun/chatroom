90ee830e829309c3744f8007b792e6aeb54137cd
yufansun <yufansun@umich.edu>
2016-03-07 23:19:30 -0500

    Can interect with server

--- public/status.js
+++ public/status.js
@@ -10,12 +10,8 @@ io.on('connection', function(socket){
 	//socket.emit('initial',{id: 1, Name: "Ren Lu",Status: "true"});
 	socket.on('myID',function(name){
 		console.log("Received request from "+name);
-		var online = stable.has(Number(coming_id));
 		socket.emit('initialStatus',contactStatus);
 		console.log("Initial Status sent!")
-	});
-	socket.on('1',function(Name){
-		console.log("1: "+Name);
 	});
 	socket.on('chat message',function(msg){
 		console.log("Received chat message: "+msg);
